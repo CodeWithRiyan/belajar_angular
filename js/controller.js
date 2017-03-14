36cea2da83354bc1e29330300fe2bbbc93934784
Cool1128 <riyan.stmik28@gmail.com>
2017-03-14 19:11:02 +0700

    my first angular

---
+++ js/controller.js
@@ -0,0 +1,5 @@
+angular.module("uli",[]).controller("li",function($scope){
+  $scope.names = [{name:"Riyan",address:"Pekalongan"},
+                  {name:"James",address:"English"},
+                {name:"Joko",address:"Solo"}];
+});
